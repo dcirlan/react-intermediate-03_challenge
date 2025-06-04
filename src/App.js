90cf2dcbd44e98e2d7ec5028827529399fd563c2
Denis <kirlan@hotmail.fr>
2025-06-04 14:54:38 +0200

    feat: implémentation des useEffect pour la gestion de la connexion et déconnexion

--- src/App.js
+++ src/App.js
@@ -3,18 +3,35 @@ import Message from './Message';
 import './style.css';
 
 export default function App() {
+  const [email, setEmail] = useState('');
+  const [isUserLoggedIn, setIsUserLoggedIn] = useState(false);
+
+  useEffect(() => {
+    console.log(`email has changed`);
+  }, [email]);
+
+  useEffect(() => {
+    console.log(`isUserLoggedIn has changed to: ${isUserLoggedIn}`);
+  }, [isUserLoggedIn]);
+
   const handleSubmit = e => {
     e.preventDefault();
+    setIsUserLoggedIn(true);
   };
+
+  const handleLogout = () => {
+    setIsUserLoggedIn(false);
+  }
 
   return (
     <div className="App">
       <form onSubmit={handleSubmit}>
         <label htmlFor="email">Email : </label>
-        <input type="text" placeholder="elon@spacex.com" value="" />
+        <input type="text" placeholder="elon@spacex.com" value={email} onChange={e => setEmail(e.target.value)} />
         <input type="submit" value="submit" />
       </form>
-      <button>Lougout</button>
+      <button onClick={handleLogout}>Logout</button>
+      {isUserLoggedIn && <Message />}
     </div>
   );
 }
--- src/Message.js
+++ src/Message.js
@@ -1,5 +1,15 @@
 import React, { useEffect, useState } from 'react';
 
 export default function Message() {
+  useEffect(() => {
+    console.log(`user is logged in`);
+  }, []);
+
+  useEffect(() => {
+    return () => {
+      console.log(`Bye !`);
+    }
+  }, []);
+
   return <h1>User is logged in</h1>;
 }
